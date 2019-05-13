0a3a36ab4ee9d232480fd46ce2a65ae7403dc835
KHwang9883 <huangyf9883@gmail.com>
2019-05-13 20:14:15 +0800

    Minor fix

--- js/sw-registration.js
+++ js/sw-registration.js
@@ -48,7 +48,7 @@ if(navigator.serviceWorker){
     if(data.command == "UPDATE_FOUND"){
       console.log("UPDATE_FOUND_BY_SW", data);
       createSnackbar({
-        message: "Content updated.",
+        message: "内容已更新。",
         actionText:"refresh",
         action: function(e){location.reload()}
       })
